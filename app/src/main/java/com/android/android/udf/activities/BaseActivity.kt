e2d5f7322c9b5b37b4f8aed31d29865b23b378ed
Suren Rodrigo <surenr@99x.lk>
2019-08-06 11:33:18 +0530

    Refactored base code

--- app/src/main/java/com/android/android/udf/activities/BaseActivity.kt
+++ app/src/main/java/com/android/android/udf/activities/BaseActivity.kt
@@ -3,23 +3,18 @@ package com.android.android.udf.activities
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
-import com.android.android.udf.Actions.BaseAction
-import com.android.android.udf.Actions.RemoveStateStatus
 import com.android.android.udf.dialogs.LoaderDialog
-import com.android.android.udf.state.ActionStatus
-import com.android.android.udf.state.AppState
-import com.android.android.udf.state.getStateFlowStatusBySession
+import com.android.android.udf.store.Base
 import com.android.android.udf.store.appStore
-import org.rekotlin.StoreSubscriber
 import java.util.*
 
-abstract class BaseActivity : AppCompatActivity(), StoreSubscriber<AppState> {
+abstract class BaseActivity : AppCompatActivity(), Base {
 
-    protected val state = appStore.state
+    override val state = appStore.state
     lateinit var loaderDialog: LoaderDialog
     //TODO: Define any other dialogs you might need in the application such as Alert Dialogs, Permission Dialog etc.
 
-    private var actionSessionIds: ArrayList<String> = ArrayList<String>()
+    override var actionSessionIds: ArrayList<String> = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,19 +22,7 @@ abstract class BaseActivity : AppCompatActivity(), StoreSubscriber<AppState> {
         //TODO: Instantiate other dialogs here.
     }
 
-    fun getActionId(): String {
-        val actionId =  UUID.randomUUID().toString()
-        actionSessionIds.add(actionId)
-        return actionId
-    }
-
-    fun dispatchAction(action: BaseAction, showLoader: Boolean = true) {
-        if(showLoader)
-            loaderDialog.showDialog()
-        appStore.dispatch(action)
-    }
-
-    private fun hideLoader() {
+    override fun hideLoader() {
         val handler = Handler()
         handler.postDelayed({
             if (::loaderDialog.isInitialized) {
@@ -48,25 +31,9 @@ abstract class BaseActivity : AppCompatActivity(), StoreSubscriber<AppState> {
         }, 50)
     }
 
-    override fun newState(state: AppState) {
-        val ids: ArrayList<String> = this.actionSessionIds.clone() as ArrayList<String>
-        ids.forEach { updateActivity(state, it) }
+    override fun showLoader() {
+        loaderDialog.showDialog()
     }
-
-    private fun updateActivity(state: AppState, actionId: String) {
-        getStateFlowStatusBySession(state, actionId)?.let { action ->
-            if (action.status == ActionStatus.COMPLETED) {
-                if (onStateUpdate(state, action))
-                    hideLoader()
-                appStore.dispatch(RemoveStateStatus.Perform(actionId, getActionId()))
-            } else if (action.status == ActionStatus.ERROR) {
-                onError(action)
-                hideLoader()
-                appStore.dispatch(RemoveStateStatus.Perform(actionId, getActionId()))
-            }
-        }
-    }
-
 
     override fun onStart() {
         super.onStart()
@@ -77,7 +44,4 @@ abstract class BaseActivity : AppCompatActivity(), StoreSubscriber<AppState> {
         super.onStop()
         appStore.unsubscribe(this)
     }
-
-    abstract fun onStateUpdate(state: AppState, action: BaseAction): Boolean
-    abstract fun onError(action: BaseAction)
 }
---
+++ app/src/main/java/com/android/android/udf/store/Base.kt
@@ -0,0 +1,61 @@
+package com.android.android.udf.store
+
+import android.os.Handler
+import android.util.Log
+import com.android.android.udf.Actions.BaseAction
+import com.android.android.udf.Actions.RemoveStateStatus
+import com.android.android.udf.state.ActionStatus
+import com.android.android.udf.state.AppState
+import com.android.android.udf.state.getStateFlowStatusBySession
+import org.rekotlin.StoreSubscriber
+import java.util.*
+
+interface  Base : StoreSubscriber<AppState> {
+    val state: AppState
+    var actionSessionIds: ArrayList<String>
+
+    fun getActionId(): String {
+        val actionId =  UUID.randomUUID().toString()
+        actionSessionIds.add(actionId)
+        return actionId
+    }
+
+    fun dispatchAction(action: BaseAction, showLoader: Boolean = true) {
+        if(showLoader)
+            showLoader()
+        appStore.dispatch(action)
+    }
+
+    fun showLoader() {
+        Log.i("show loader", "Loader Shown")
+    }
+
+    fun hideLoader() {
+        val handler = Handler()
+        handler.postDelayed({
+           Log.i("hide loader", "Loader Hidden")
+        }, 50)
+    }
+
+    override fun newState(state: AppState) {
+        val ids: ArrayList<String> = this.actionSessionIds.clone() as ArrayList<String>
+        ids.forEach { updateActivity(state, it) }
+    }
+
+    private fun updateActivity(state: AppState, actionId: String) {
+        getStateFlowStatusBySession(state, actionId)?.let { action ->
+            if (action.status == ActionStatus.COMPLETED) {
+                if (onStateUpdate(state, action))
+                    hideLoader()
+                appStore.dispatch(RemoveStateStatus.Perform(actionId, getActionId()))
+            } else if (action.status == ActionStatus.ERROR) {
+                onError(action)
+                hideLoader()
+                appStore.dispatch(RemoveStateStatus.Perform(actionId, getActionId()))
+            }
+        }
+    }
+
+    fun onStateUpdate(state: AppState, action: BaseAction): Boolean
+    fun onError(action: BaseAction)
+}

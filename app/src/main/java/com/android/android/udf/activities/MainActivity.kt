592cc9037fc719324877252065f482182eb6f0d0
Suren Rodrigo <surenr@99x.lk>
2019-08-08 18:25:20 +0530

    wired the calculator logic to buttons with ease

--- app/src/main/java/com/android/android/udf/activities/MainActivity.kt
+++ app/src/main/java/com/android/android/udf/activities/MainActivity.kt
@@ -2,8 +2,7 @@ package com.android.android.udf.activities
 
 import android.os.Bundle
 import android.util.Log
-import com.android.android.udf.Actions.BaseAction
-import com.android.android.udf.Actions.CalcAdd
+import com.android.android.udf.Actions.*
 import com.android.android.udf.R
 import com.android.android.udf.state.AppState
 import com.android.android.udf.state.getCalResult
@@ -12,10 +11,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : BaseActivity() {
     override fun onStateUpdate(state: AppState, action: BaseAction): Boolean {
         when(action){
-            is CalcAdd ->{
-                calResult.text = getCalResult(state).toString()
-            }
+           // TODO: Handle any special actions here
         }
+        calResult.text = getCalResult(state).toString()
         return true
     }
 
@@ -26,11 +24,29 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         btnAdd.setOnClickListener {
             val inputNumber = numberInput.text.toString().toInt()
             val existingNumber = if (calResult.text.toString().isEmpty()) 0 else calResult.text.toString().toInt()
             dispatchAction(CalcAdd.Perform(existingNumber, inputNumber, getActionId()))
-            Log.i("clicked", "Clicked Clicked Clicked")
+        }
+
+        btnSubtract.setOnClickListener {
+            val inputNumber = numberInput.text.toString().toInt()
+            val existingNumber = if (calResult.text.toString().isEmpty()) 0 else calResult.text.toString().toInt()
+            dispatchAction(CalcSubtract.Perform(existingNumber, inputNumber, getActionId()))
+        }
+
+        btnMultiply.setOnClickListener {
+            val inputNumber = numberInput.text.toString().toInt()
+            val existingNumber = if (calResult.text.toString().isEmpty()) 0 else calResult.text.toString().toInt()
+            dispatchAction(CalcMultiply.Perform(existingNumber, inputNumber, getActionId()))
+        }
+
+        btnDivision.setOnClickListener {
+            val inputNumber = numberInput.text.toString().toInt()
+            val existingNumber = if (calResult.text.toString().isEmpty()) 0 else calResult.text.toString().toInt()
+            dispatchAction(CalcDivide.Perform(existingNumber, inputNumber, getActionId()))
         }
     }
 }
